53690a69b897c50ebb4556aa4beb54451cfeb066
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-08-17 13:36:34 -0500

    burger-queen-pwa

--- burger-queen/src/tests/components/login/logIn.test.js
+++ burger-queen/src/tests/components/login/logIn.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import FormLogin from '../../../components/login/FormLogin';
-import userEvent from '@testing-library/user-event';
-
 
 const user = {
-    userEmail: 'example@gmail.com',
-    userContraseña: '13142dsg',
-  }
+  userEmail: 'example@gmail.com',
+  userContraseña: '13142dsg',
+};
 
 const error = {
   userEmail: false,
@@ -16,14 +14,12 @@ const error = {
 
 describe('Render', () => {
   test('Debería encontrar "Inicia sesión" en el componente', () => {
-    render (<FormLogin user= {user} error={error} />);
+    render(<FormLogin user={user} error={error} />);
     screen.getByText('Inicia sesión');
-  })
+  });
 
   test('Debería retornar el valor de name', () => {
-    render(<FormLogin user= {user} error={error} />);
+    render(<FormLogin user={user} error={error} />);
     expect(user.userEmail).toBe('example@gmail.com');
   });
 });
-
-
